6f081469705c1aceac499415be92c63ee7c609bc
mmatz75 <69707077+mmatz75@users.noreply.github.com>
2023-12-12 13:47:19 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  // suppress 'GenerateSW has been called multiple times' in dev mode
+  disable: process.env.NODE_ENV === 'development',
+})
+
+module.exports = withPWA({
+  //next.js config
+  reactStrictMode: true,
+})
